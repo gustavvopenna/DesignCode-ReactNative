82ddaa095e1266264c118546b90b4c6d3162dae2
gustavvopenna <gustavo.pema@gmail.com>
2019-07-10 16:27:01 -0500

    Create NavTitle and Card components

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text } from 'react-native'
+import styled from 'styled-components'
+import Card from './components/Cards'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My first React Native App!</Text>
-    </View>
+    <Container>
+      <TitleBar>
+        <Avatar source={require('./assets/avatar.jpg')} />
+        <Title>Welcome back,</Title>
+        <Name>Gustavo</Name>
+      </TitleBar>
+      <Subtitle>Continue learning</Subtitle>
+      <Card />
+    </Container>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background-color: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`
